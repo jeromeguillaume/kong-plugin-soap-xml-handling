c0c2a6239723de2c38d400a7a6ecb582b52c7734
jeromeguillaume <jerome.guillaume@live.fr>
2024-09-06 13:55:37 +0200

    Pongo

--- pongo.sh
+++ pongo.sh
@@ -1,8 +1,23 @@
-#KONG_IMAGE=jeromeguillaume/kong-saxon:3.7.1.1 pongo build
+#-----------------------------------------------------------------------------
+# Rebuild the pongo image
+#  Required if the 'KONG_IMAGE' has been rebuilt w/o changing its tag version
+#-----------------------------------------------------------------------------
+# KONG_IMAGE=jeromeguillaume/kong-saxon:3.7.1.1 pongo build
 
-#KONG_IMAGE=kong/kong-gateway pongo run --lpath=$PWD/spec/common
+#------------------------------------------------
+# Execute the tests with the Kong standard image
+#   Doesn't work with the Saxon tests
+#------------------------------------------------
+# KONG_IMAGE=kong/kong-gateway pongo run --lpath=$PWD/spec/common
+
+#-------------------------------------------------------------
+# Execute the tests with the customized Kong image with Saxon
+#-------------------------------------------------------------
 KONG_IMAGE=jeromeguillaume/kong-saxon:3.7.1.1 pongo run --lpath=$PWD/spec/common
 
+#---------------------------------------------------------
+# Loop tests
+#---------------------------------------------------------
 #COUNTER=0
 #while true
 #do
